0fefb0f0bf2cfc1ef4e7093a31ebb1f5651e3a23
mcbarinov <mcbarinov@proton.me>
2025-01-21 11:39:38 +0300

    Set price to zero if no coingecko_id #29

--- src/mm_balance/price.py
+++ src/mm_balance/price.py
@@ -1,14 +1,15 @@
+from collections import defaultdict
 from decimal import Decimal
 
 import pydash
-from mm_std import fatal, hr
+from mm_std import hr
 from mm_std.random_ import random_str_choice
 
 from mm_balance.config import Config, Group
 from mm_balance.constants import RETRIES_COINGECKO_PRICES, TICKER_TO_COINGECKO_ID
 
 
-class Prices(dict[str, Decimal]):
+class Prices(defaultdict[str, Decimal]):
     """
     A Prices class representing a mapping from coin names to their prices.
 
@@ -20,29 +21,29 @@ class Prices(dict[str, Decimal]):
 def get_prices(config: Config) -> Prices:
     result = Prices()
 
-    coins = pydash.uniq([group.ticker for group in config.groups])
-    coingecko_ids = pydash.uniq([get_coingecko_id(group) for group in config.groups])
+    coingecko_map: dict[str, str] = {}  # ticker -> coingecko_id
 
-    url = f"https://api.coingecko.com/api/v3/simple/price?ids={",".join(coingecko_ids)}&vs_currencies=usd"
+    for group in config.groups:
+        coingecko_id = get_coingecko_id(group)
+        if coingecko_id:
+            coingecko_map[group.ticker] = coingecko_id
+
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids={','.join(coingecko_map.values())}&vs_currencies=usd"
     for _ in range(RETRIES_COINGECKO_PRICES):
         res = hr(url, proxy=random_str_choice(config.proxies))
+        print(res)
         if res.code != 200:
             continue
 
-        for idx, coin in enumerate(coins):
-            if coingecko_ids[idx] in res.json:
-                result[coin] = Decimal(str(pydash.get(res.json, f"{coingecko_ids[idx]}.usd")))
-            else:
-                fatal("Can't get price for {coin} from coingecko, coingecko_id={coingecko_ids[idx]}")
+        for ticker, coingecko_id in coingecko_map.items():
+            if coingecko_id in res.json:
+                result[ticker] = Decimal(str(pydash.get(res.json, f"{coingecko_id}.usd")))
+        break
 
     return result
 
 
-def get_coingecko_id(group: Group) -> str:
+def get_coingecko_id(group: Group) -> str | None:
     if group.coingecko_id:
         return group.coingecko_id
-    coingecko_id = TICKER_TO_COINGECKO_ID.get(group.ticker)
-    if coingecko_id:
-        return coingecko_id
-
-    fatal(f"Can't get coingecko_id for {group.ticker}. Please add coingecko_id to the config.")
+    return TICKER_TO_COINGECKO_ID.get(group.ticker)
